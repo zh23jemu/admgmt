7e9fbaca3e20429ef267c4a2aaa8c98a3d04f208
zh23jemu <41720280+zh23jemu@users.noreply.github.com>
2018-11-09 14:48:39 +0800

    initial version

---
+++ FindAndMove-ADUsers.ps1
@@ -0,0 +1,23 @@
+﻿$notinad = Get-Content D:\notinadd.txt
+
+$notinad | ogv
+
+foreach ($item in $notinad)
+{
+    $sam = ($item -split "@")[0]
+    
+    try
+    {
+        $aduser = get-aduser $sam -ErrorAction stop
+        Move-ADObject -Identity $aduser.DistinguishedName -TargetPath "OU=CADENA,OU=ADEN-Users,DC=CHOADEN,DC=COM"
+    }
+    catch [exception]
+    {
+        #"no such user: " + $sam
+    }
+
+}
+
+
+get-aduser -SearchBase "OU=CADENA,OU=ADEN-Users,DC=CHOADEN,DC=COM" -Filter * | Export-Excel -Path "d:\cadena.xlsx" -AutoSize
+get-aduser phan.lien
